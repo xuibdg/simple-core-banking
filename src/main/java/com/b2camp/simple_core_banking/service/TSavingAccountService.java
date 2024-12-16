29cdd95a82320ad70d4e5796e4d5b0325c0a6678
Sandi Vanbiana <vanbianasandi@gmail.com>
2024-12-16 21:43:47 +1100

    add API for method add read(get) TSavingAccount

--- src/main/java/com/b2camp/simple_core_banking/service/TSavingAccountService.java
+++ src/main/java/com/b2camp/simple_core_banking/service/TSavingAccountService.java
@@ -1,4 +1,14 @@
 package com.b2camp.simple_core_banking.service;
 
+import com.b2camp.simple_core_banking.dto.TSavingAccountResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public interface TSavingAccountService {
+    List<TSavingAccountResponse>readTsavingAccount(String accountNumber);
+
+    Optional<TSavingAccountResponse>findBySavingAccountId(String savingAccountId);
 }
