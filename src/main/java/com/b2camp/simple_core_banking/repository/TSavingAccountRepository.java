4de51f97a7c8e29ec14dcd5095822a16c4f53da0
Sandi Vanbiana <vanbianasandi@gmail.com>
2024-12-19 17:46:03 +1100

    add Transactional isolation REPEATABLE_READ,add propagation REQUIRED and logging log.info

--- src/main/java/com/b2camp/simple_core_banking/repository/TSavingAccountRepository.java
+++ src/main/java/com/b2camp/simple_core_banking/repository/TSavingAccountRepository.java
@@ -3,5 +3,8 @@ package com.b2camp.simple_core_banking.repository;
 import com.b2camp.simple_core_banking.entity.TSavingAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TSavingAccountRepository extends JpaRepository<TSavingAccount, String> {
+    List<TSavingAccount> findAllByAccountNumberAndIsDeletedIsFalse(String accountNumber);
 }
